70e16ff2fc91eb3c82513aa8d7c7fb1172514e2f
woshixuanfeng <56616854+woshixuanfeng@users.noreply.github.com>
2020-02-27 16:48:53 +0800

    修改内容

--- lishangwanglai/src/router/index.js
+++ lishangwanglai/src/router/index.js
@@ -1,21 +1,22 @@
 import React,{Component} from 'react'
 import {Switch,Redirect,Route} from 'react-router-dom'
-import Header from '../component/header/header'
-import Home from '../component/home/Home'
-import Director from '../component/director/director'
-import Manage from '../component/Manage/Manage '
-import About from '../component/about/about'
+import Header from '../component/Header/header'
+import shouye from '../component/shouye/shouye'
+import lizhang from '../component/lizhang/lizhang'
+import lishi from '../component/lishi/lishi'
+import wode from '../component/wode/wode'
 import Index from './index.css'
 function router() { 
     return(
         <div id="box">
             <Header></Header>
+           
             <Switch>
-                <Route exact path='/' component={Home}/>
-                <Route path='/manage' component={Manage}/>
-                <Route path='/director' component={Director}/>
-                <Route path='/about' component={About}/>
-                <Route component={Home}/>
+                <Route exact path='/' component={shouye}/>
+                <Route path='/lishi' component={lishi}/>
+                <Route path='/lizhang' component={lizhang}/>
+                <Route path='/wode' component={wode}/>
+                <Route component={Header}/>
             </Switch>
         </div>
     )
